554806d6572ed931039040a5d60cc16d7af28063
Gopal kataria <gopalkataria@outlook.com>
2021-03-08 14:46:06 +0530

    removed crap stuff ,

--- art.py
+++ art.py
@@ -4,10 +4,13 @@ image = cv2.imread("image.jpg")
 #converting image to grayscale for my algo to work 
 bwImage = cv2.cvtColor( image , cv2.COLOR_RGB2GRAY )
 
-chars = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~i!lI;:,\\\"^`\". "
+#chars = "$@B%8&WM#*oahkbwmZO0QLJDEXznxrjft/\|()1{}[]?-_+~i!lI;:,\\\"^`\". "
+chars = "$@B%8&WM#*oaihkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~i!lI;:,\\\"^`\".  "
+#chars  = "$@ZLIhlai{}\-:. "
 
 # reducing number of colors in image according to number of available characters
-rImage =  bwImage // 4  
+rImage =  bwImage // 3.6
+
 
 #freeing up some memory
 del(bwImage) 
@@ -17,8 +20,8 @@ nImage = []
 ncol = ""
 for r in rImage :
     for col in r :
-        ncol += chars[col]
-        ncol += chars[col]
+        ncol += chars[int(col) ]
+        ncol += chars[int(col)]
     nImage.append(ncol)
     ncol = ""
 
